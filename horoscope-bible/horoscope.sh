55f4cdbe8918a58fba61aceb8053898f66a57406
Jason Macias <jasonsmacias@gmail.com>
2020-09-22 15:26:42 -0400

    changed docker image to alpine w/ curl, bash, jq, started on pipeline to run existing task

--- horoscope-bible/horoscope.sh
+++ horoscope-bible/horoscope.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 # A bash script for Horoscope/Bible concourse pipeline
-# To use: Alpine
 
-# below to get jq, commented out for now
-# RUN apk add --update --no-cache curl jq
+# sudo apt update
+# sudo apt-get install jq
 
 curl -X POST \
 "https://aztro.sameerkumar.website/?sign=$1&day=today"  > result$(date +%d-%m-%y).json
 ls -l
-cat result$(date +%d-%m-%y).json
+
+cat result$(date +%d-%m-%y).json | jq .
