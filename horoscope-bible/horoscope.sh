12fefebb3774a8a4ab2a026a9f7254f4c362dc6d
mzmzmz <44877972+JasonSMacias@users.noreply.github.com>
2020-09-24 17:37:46 -0400

    updates and added to timed pipeline basic functionality

--- horoscope-bible/horoscope.sh
+++ horoscope-bible/horoscope.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-# A bash script for Horoscope/Bible concourse pipeline
 
-# sudo apt update
-# sudo apt-get install jq
+# A bash script for Horoscope/Bible concourse pipeline
 
 curl -X POST \
 "https://aztro.sameerkumar.website/?sign=$1&day=today"  > result$(date +%d-%m-%y).json
---
+++ timed/timed.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+#############################################
+# Author: Jason Macias
+# Version: v1.0.0
+# Date: 9/24/2020
+# Description: prints sentence with current date/time
+# Usage: no arguments
+#############################################
+
+echo "Hello, I ran on $(date +%A) $(date +%B) $(date +%d), $(date +%y), at $(date +%r)."
