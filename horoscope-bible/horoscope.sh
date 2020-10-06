603311dd452ea88b84fa238354eb56148863cdff
mzmzmz <44877972+JasonSMacias@users.noreply.github.com>
2020-10-06 12:06:34 -0400

    redid horoscope.sh

--- horoscope-bible/horoscope.sh
+++ horoscope-bible/horoscope.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 # A bash script for Horoscope/Bible concourse pipeline
-
-curl -X POST \
-"https://aztro.sameerkumar.website/?sign=$1&day=today"  > result$(date +%d-%m-%y).json
-ls -l
-
-cat result$(date +%d-%m-%y).json | jq .
-
 if [[ ! -d horoscope-json ]]; then
     mkdir horoscope-json
-    mv result$(date +%d-%m-%y).json ./horoscope-json/result$(date +%d-%m-%y).json || echo "mkdir or move fail"
 fi
+
+curl -X POST \
+"https://aztro.sameerkumar.website/?sign=$1&day=today"  > horoscope-json/result$(date +%d-%m-%y).json
+ls -l
+
+
 ls -l horoscope-json
+cat horoscope-json/result$(date +%d-%m-%y).json | jq .
+
 echo "horoscope.sh completed"
