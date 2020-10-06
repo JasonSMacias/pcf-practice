55585eb9e355b6f61b8f1baddd2a86e54564f301
mzmzmz <44877972+JasonSMacias@users.noreply.github.com>
2020-10-06 11:52:03 -0400

    debug output

--- horoscope-bible/horoscope.sh
+++ horoscope-bible/horoscope.sh
@@ -11,4 +11,5 @@ cat result$(date +%d-%m-%y).json | jq .
 if [[ ! -d horoscope-json ]]; then
     mkdir horoscope-json && mv result$(date +%d-%m-%y).json ./horoscope-json || echo "mkdir or move fail"
 fi
+ls -l horoscope-json
 echo "horoscope.sh completed"
