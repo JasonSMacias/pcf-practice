2149812ca5a28b3f448377942a900f8d6f7058ae
mzmzmz <44877972+JasonSMacias@users.noreply.github.com>
2020-10-08 12:23:22 -0400

    horoscope.sh updated as described in last commit

--- horoscope-bible/horoscope.sh
+++ horoscope-bible/horoscope.sh
@@ -8,8 +8,17 @@ fi
 echo "environment variables:"
 env
 
+SIGNS=(aries taurus gemini cancer leo virgo libra scorpio sagittarius capricorn aquarius pisces)
+sign_arg=$1
+for sign in "${SIGNS[@]}"
+do
+    if [[ $sign = $SIGN_NAME ]]; then
+        sign_arg=$sign
+    fi
+done
+
 curl -X POST \
-"https://aztro.sameerkumar.website/?sign=$1&day=today"  > horoscope-json/result$(date +%d-%m-%y).json
+"https://aztro.sameerkumar.website/?sign=$sign_arg&day=today"  > horoscope-json/result$(date +%d-%m-%y).json
 ls -l
 
 
